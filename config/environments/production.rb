30f95fa3e2829ff065bc12f082c1cc5ca8bd7f73
Ben Rothman <bensrothman@gmail.com>
2013-01-22 13:18:13 -0600

    add www to host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Giftique::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'giftique.tk' }
+  config.action_mailer.default_url_options = { :host => 'www.giftique.tk' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
