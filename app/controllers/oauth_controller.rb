6bfd4e3021d9edfe5316bc2c773094bee5b207dd
Ben Rothman <bensrothman@gmail.com>
2013-01-25 21:23:40 -0600

    transfer to private github

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,6 +3,6 @@ class OauthController < ApplicationController
 	def redirect
 	  session[:access_token] = Koala::Facebook::OAuth.new('http://giftique.herokuapp.com/oauth').get_access_token(params[:code]) if params[:code]
 
-	  redirect_to session[:access_token] ? 'home#index' : '/fberror.html', :notice => "Successful FB Authorization"
+	  redirect_to session[:access_token] ? root : '/fberror.html', :notice => "Successful FB Authorization"
 	end
 end
